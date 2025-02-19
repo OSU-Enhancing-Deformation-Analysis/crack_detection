352c8a870c626a2752541af66fdee93f16ac381d
Jason Lien <107185131+Jason1ien@users.noreply.github.com>
2025-02-19 15:35:24 -0800

    Included Image Cropping for SEM image labels

--- crack_detect_batch.py
+++ crack_detect_batch.py
@@ -6,151 +6,145 @@ import os
 np.set_printoptions(threshold=sys.maxsize)
 
 class CrackDetect:
-    def __init__(self, crack_darkness=40, fill_threshhold=2, sharpness=50, resolution=3, amount=1):
+    def __init__(self, crack_darkness=40, fill_threshhold=2, sharpness=50, resolution=3, amount=1, crop_pixels=20):
         self.darkness = crack_darkness
         self.threshhold = fill_threshhold
         self.sharpness = sharpness
         self.resolution = resolution
         self.amount = amount
+        self.crop_pixels = crop_pixels
 
     def __process_image(self, image_path):
         # Read the image
         image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-
+        # Crop out the bottom crop_pixels rows first
+        image = image[:-self.crop_pixels, :]
+        
         # Apply Gaussian blur to reduce noise
         blurred = cv2.GaussianBlur(image, (5, 5), 0)
-
-        # Convert to boolean black and white image
+        
+        # Convert to a binary image (black and white) based on darkness threshold
         dark_mask = cv2.inRange(blurred, 0, self.darkness)
-
-        # dilate to fill crack area
+        
+        # Dilate to fill crack area
         kernel = np.ones((5, 5), np.uint8)
         dilated = cv2.dilate(dark_mask, kernel, iterations=self.threshhold)
-
-        # remove holes in crack area
+        
+        # Remove holes in crack area
         filled_img = self.__fill_holes(dilated)
-
-        # erode to seperate crack from noise
+        
+        # Erode to separate crack from noise
         kernel = np.ones((3, 3), np.uint8)
         eroded = cv2.erode(filled_img, kernel, iterations=1)
-
-        # remove extra noise spots in non crack area
+        
+        # Remove extra noise spots in non-crack area
         cleaned_img = self.__remove_spots(eroded)
-
+        
         return cleaned_img
 
     def __remove_spots(self, image):
-        # Fill holes
-        # Invert the binary image
+        # In this context, no inversion is done as the input is already a binary image
         inverted = image
-
+        
         # Label connected components in the inverted image
         num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(inverted)
-
-        # Create a copy of the original image to modify
+        
+        # Create a copy of the image to modify
         filled_img = image.copy()
-
+        
         # Order labels by area size, excluding background
         sorted_labels = np.argsort([x[cv2.CC_STAT_AREA] for x in stats[1:]]) + 1
-
+        
         for i in range(1, num_labels):
             if i in sorted_labels[:-self.amount]:
-                # removed the spot by setting to black
+                # Remove the spot by setting it to black
                 filled_img[labels == i] = 0
-
+                
         return filled_img
 
     def __fill_holes(self, image):
-        # Fill holes
         # Invert the binary image
         inverted = cv2.bitwise_not(image)
-
+        
         # Label connected components in the inverted image
         num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(inverted)
-
-        # Create a copy of the original image to modify
+        
+        # Create a copy of the image to modify
         filled_img = image.copy()
-
+        
         for i in range(1, num_labels):  # Skip the background label (0)
-            # Get the size of the connected component
             area = stats[i, cv2.CC_STAT_AREA]
-
-            # Define a threshold to identify holes (e.g., ignore very large black regions)
-            max_hole_area = 20000  # Adjust this threshold based on your image
+            max_hole_area = 20000  # Adjust threshold as needed
             if area < max_hole_area:
-                # Fill the hole: set all pixels in this component to white in the original image
-                    filled_img[labels == i] = 255
-
+                # Fill the hole by setting pixels to white
+                filled_img[labels == i] = 255
+                
         return filled_img
 
-    # returns True if image contains a crack
+    # Returns True if image contains a crack
     def has_crack(self, image_path):
         cleaned_img = self.__process_image(image_path)
-
+        
         # Label connected components
         num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(cleaned_img)
-
-        # Check if any crack (component) reaches required size
+        
+        # Check if any crack (component) reaches the required size
         for i in range(1, num_labels):
             width = stats[i, cv2.CC_STAT_WIDTH]
             height = stats[i, cv2.CC_STAT_HEIGHT]
-
             min_size = 100
-            if(width > min_size or height > min_size):
+            if width > min_size or height > min_size:
                 return True
-            
         return False
 
-    # given an image, returns a set of polygons that outline a crack on the image.
-    # also saves and shows a preview image, and if specified, a DICE subset file 
+    # Given an image, returns a set of polygons that outline a crack.
+    # Also saves and shows a preview image, and if specified, a DICE subset file.
     def outline_crack(self, image_path, save_subset=False):
+        # Process the image
         cleaned_img = self.__process_image(image_path)
-            # smooth edges
+        
+        # Smooth edges and generate a mask
         blurred = cv2.GaussianBlur(cleaned_img, (13, 13), 0)
         smooth_mask = cv2.inRange(blurred, self.sharpness, 255)
-
-        # get border contour of area
+        
+        # Get the border contours of the area
         contours, _ = cv2.findContours(smooth_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+        
         # Approximate the contours to polygons
         approx_polygons = []
         for i in range(self.amount):
-            # epsilon = RESOLUTION * cv2.arcLength(contours[i], True)  # Adjust epsilon for precision
             epsilon = self.resolution
             approx_polygons.append(cv2.approxPolyDP(contours[i], epsilon, True))
-
-        # Draw polygon on image for preview
+        
+        # For preview, re-read and crop the original image
         original_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        original_image = original_image[:-self.crop_pixels, :]
         preview_image = cv2.cvtColor(original_image, cv2.COLOR_GRAY2BGR)
         polygons = cv2.polylines(preview_image, approx_polygons, True, (0, 255, 0), 2)
-
+        
         file_name = os.path.basename(image_path).split(".")[0] + "_outline.jpg"
-        folder_name = os.path.dirname(image_path) + "_results" 
+        folder_name = os.path.join(os.path.dirname(image_path) + "_results")
         if not os.path.isdir(folder_name):
             os.mkdir(folder_name)
-        cv2.imwrite(folder_name + "/" + file_name, polygons)
-
-        if(save_subset==True):
-            #     # Save polygon as DICe subset file
-
-            # area of interest is the entire image
+        cv2.imwrite(os.path.join(folder_name, file_name), polygons)
+        
+        if save_subset:
             height, width = original_image.shape
             aoi = """0 0
-                        0 {0}
-                        {1} {0}
-                        {1} 0""".format(height, width)
+0 {0}
+{1} {0}
+{1} 0""".format(height, width)
             
-            # exclusions are the polygons generated
             exclusions = ''
             for p in approx_polygons:
                 poly_string = ''.join(''.join(str(x[0,0]) + ' ' + str(x[0,1])) + '\n\t\t\t\t' for x in p)[:-5]
                 exclusions += """
-        begin polygon 
-            begin vertices
-                {}
-            end vertices
-        end polygon""".format(poly_string)
-
+    begin polygon 
+        begin vertices
+            {}
+        end vertices
+    end polygon""".format(poly_string)
+            
             output = """# Crack detect output
 begin region_of_interest
     begin boundary
@@ -165,26 +159,32 @@ begin region_of_interest
 end region_of_interest""".format(aoi, exclusions)
             with open(file_name + "_subset.txt", "w") as file:
                 file.write(output)
+                
         return approx_polygons
 
-    #creates outlines for all images in a folder (does not show preview or save subset file)
+    # Creates outlines for all images in a folder
     def outline_crack_batch(self, folder_path):
-        files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if (os.path.isfile(os.path.join(folder_path, f)) and (os.path.splitext(f)[1] == ".tif" or os.path.splitext(f)[1] == ".tiff"))]
+        files = [os.path.join(folder_path, f) for f in os.listdir(folder_path)
+                 if os.path.isfile(os.path.join(folder_path, f)) and 
+                 (os.path.splitext(f)[1] in [".tif", ".tiff"])]
         for f in files:
-            if(self.has_crack(f)):
-                self.outline_crack(f) 
-
-        
+            if self.has_crack(f):
+                self.outline_crack(f)
 
 def main():
+    # Check command line arguments
+    if len(sys.argv) < 2:
+        print("Usage: python crack_detect_batch.py <folder_path> [crack_darkness] [fill_threshhold] [crop_pixels]")
+        sys.exit(1)
 
-    detector = None
-    if(len(sys.argv) > 2):
-        detector = CrackDetect(crack_darkness=int(sys.argv[2]), fill_threshhold=int(sys.argv[3]))
-    else:
-        detector = CrackDetect()
-   
-    detector.outline_crack_batch(sys.argv[1])
+    folder_path = sys.argv[1]
+    # Optional parameters with defaults
+    crack_darkness = int(sys.argv[2]) if len(sys.argv) > 2 else 40
+    fill_threshhold = int(sys.argv[3]) if len(sys.argv) > 3 else 2
+    crop_pixels = int(sys.argv[4]) if len(sys.argv) > 4 else 60
+
+    detector = CrackDetect(crack_darkness=crack_darkness, fill_threshhold=fill_threshhold, crop_pixels=crop_pixels)
+    detector.outline_crack_batch(folder_path)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
